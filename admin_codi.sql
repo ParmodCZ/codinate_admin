99a623ca5bc2fb5630a97ed70ab510dfa2aff68f
ParmodCZ <parmod.kumar@contriverz.com>
2019-11-17 00:03:38 +0530

    test push codetestt333

--- admin_codi.sql
+++ admin_codi.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 14, 2019 at 06:53 PM
--- Server version: 10.1.35-MariaDB
--- PHP Version: 7.2.9
+-- Generation Time: Nov 13, 2019 at 08:31 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.1.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,43 +25,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ci_sessions`
---
-
-CREATE TABLE `ci_sessions` (
-  `session_id` varchar(40) NOT NULL DEFAULT '0',
-  `ip_address` varchar(45) NOT NULL DEFAULT '0',
-  `user_agent` varchar(120) NOT NULL,
-  `last_activity` int(10) UNSIGNED NOT NULL DEFAULT '0',
-  `user_data` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `tbl_last_login`
 --
 
 CREATE TABLE `tbl_last_login` (
-  `id` bigint(20) NOT NULL,
-  `userId` bigint(20) NOT NULL,
-  `sessionData` varchar(2048) NOT NULL,
-  `machineIp` varchar(1024) NOT NULL,
-  `userAgent` varchar(128) NOT NULL,
-  `agentString` varchar(1024) NOT NULL,
-  `platform` varchar(128) NOT NULL,
-  `createdDtm` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(222) NOT NULL,
+  `userId` varchar(222) NOT NULL,
+  `sessionData` varchar(222) NOT NULL,
+  `machineIp` varchar(222) NOT NULL,
+  `userAgent` varchar(222) NOT NULL,
+  `agentString` text NOT NULL,
+  `platform` text NOT NULL,
+  `createdDtm` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_last_login`
 --
 
 INSERT INTO `tbl_last_login` (`id`, `userId`, `sessionData`, `machineIp`, `userAgent`, `agentString`, `platform`, `createdDtm`) VALUES
-(1, 1, '{\"role\":\"1\",\"roleText\":\"System Administrator\",\"name\":\"System Administrator\"}', '::1', 'Chrome 77.0.3865.120', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36', 'Windows 7', '2019-11-14 22:51:14'),
-(2, 9, '{\"role\":\"2\",\"roleText\":\"admin\",\"name\":\"pk\"}', '::1', 'Chrome 78.0.3904.97', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36', 'Windows 7', '2019-11-14 00:00:00'),
-(3, 9, '{\"role\":\"2\",\"roleText\":\"Manager\",\"name\":\"Parmod\"}', '::1', 'Chrome 77.0.3865.120', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36', 'Windows 7', '2019-11-14 23:03:07'),
-(4, 1, '{\"role\":\"1\",\"roleText\":\"System Administrator\",\"name\":\"System Administrator\"}', '::1', 'Chrome 77.0.3865.120', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36', 'Windows 7', '2019-11-14 23:04:15');
+(1, '1', '{\"role\":\"2\",\"roleText\":\"admin\",\"name\":\"pk\"}', '::1', 'Chrome 78.0.3904.97', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36', 'Windows 7', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -70,17 +53,13 @@ INSERT INTO `tbl_last_login` (`id`, `userId`, `sessionData`, `machineIp`, `userA
 --
 
 CREATE TABLE `tbl_reset_password` (
-  `id` bigint(20) NOT NULL,
-  `email` varchar(128) NOT NULL,
-  `activation_id` varchar(32) NOT NULL,
-  `agent` varchar(512) NOT NULL,
-  `client_ip` varchar(32) NOT NULL,
-  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
-  `createdBy` bigint(20) NOT NULL DEFAULT '1',
-  `createdDtm` datetime NOT NULL,
-  `updatedBy` bigint(20) DEFAULT NULL,
-  `updatedDtm` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL,
+  `email` varchar(222) NOT NULL,
+  `activation_id` varchar(222) NOT NULL,
+  `createdDtm` varchar(222) NOT NULL,
+  `agent` varchar(222) NOT NULL,
+  `client_ip` int(222) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -89,18 +68,17 @@ CREATE TABLE `tbl_reset_password` (
 --
 
 CREATE TABLE `tbl_roles` (
-  `roleId` tinyint(4) NOT NULL COMMENT 'role id',
-  `role` varchar(50) NOT NULL COMMENT 'role text'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `id` int(222) NOT NULL,
+  `role` varchar(222) NOT NULL,
+  `roleId` varchar(222) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_roles`
 --
 
-INSERT INTO `tbl_roles` (`roleId`, `role`) VALUES
-(1, 'System Administrator'),
-(2, 'Manager'),
-(3, 'Employee');
+INSERT INTO `tbl_roles` (`id`, `role`, `roleId`) VALUES
+(0, 'admin', '2');
 
 -- --------------------------------------------------------
 
@@ -109,40 +87,45 @@ INSERT INTO `tbl_roles` (`roleId`, `role`) VALUES
 --
 
 CREATE TABLE `tbl_users` (
-  `userId` int(11) NOT NULL,
-  `email` varchar(128) NOT NULL COMMENT 'login email',
-  `password` varchar(128) NOT NULL COMMENT 'hashed login password',
-  `name` varchar(128) DEFAULT NULL COMMENT 'full name of user',
-  `mobile` varchar(20) DEFAULT NULL,
-  `roleId` tinyint(4) NOT NULL,
-  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
-  `createdBy` int(11) NOT NULL,
-  `createdDtm` datetime NOT NULL,
-  `updatedBy` int(11) DEFAULT NULL,
-  `updatedDtm` datetime DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `userId` int(222) NOT NULL,
+  `username` varchar(222) NOT NULL,
+  `name` varchar(222) NOT NULL,
+  `email` varchar(222) NOT NULL,
+  `password` varchar(222) NOT NULL,
+  `roleId` varchar(222) NOT NULL DEFAULT '0',
+  `isDeleted` int(222) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_users`
 --
 
-INSERT INTO `tbl_users` (`userId`, `email`, `password`, `name`, `mobile`, `roleId`, `isDeleted`, `createdBy`, `createdDtm`, `updatedBy`, `updatedDtm`) VALUES
-(1, 'admin@example.com', '$2y$10$2dumS46BuRkxNT4VEFf78eN9czB.Y8aKoNmutuePAxSZsGSvFJQtm', 'System Administrator', '7696399515', 1, 0, 0, '2015-07-01 18:56:49', 1, '2019-11-14 18:47:49'),
-(2, 'manager@example.com', '$2y$10$2dumS46BuRkxNT4VEFf78eN9czB.Y8aKoNmutuePAxSZsGSvFJQtm', 'Manager', '9890098900', 2, 1, 1, '2016-12-09 17:49:56', 1, '2019-11-14 18:29:09'),
-(3, 'employee@example.com', '$2y$10$2dumS46BuRkxNT4VEFf78eN9czB.Y8aKoNmutuePAxSZsGSvFJQtm', 'Employee', '9890098900', 3, 0, 1, '2016-12-09 17:50:22', 3, '2018-01-04 07:58:28'),
-(9, 'pramod.kumar@contriverz.com', '$2y$10$2dumS46BuRkxNT4VEFf78eN9czB.Y8aKoNmutuePAxSZsGSvFJQtm', 'Parmod', '7696399515', 2, 0, 0, '0000-00-00 00:00:00', 1, '2019-11-14 18:22:31'),
-(10, 'dadmin@example.com', '$2y$10$OCr3Mx1BXEyU9DqLITK1wuTPcnXwYeGZ/d9AD9KZ4Fpt1qRvefG9u', 'Devesh Choudhry', '7696399516', 3, 0, 1, '2019-11-14 18:28:04', NULL, NULL);
+INSERT INTO `tbl_users` (`userId`, `username`, `name`, `email`, `password`, `roleId`, `isDeleted`) VALUES
+(1, 'pramod.kumar@contriverz.com', 'pk', 'pramod.kumar@contriverz.com', '$2y$10$2dumS46BuRkxNT4VEFf78eN9czB.Y8aKoNmutuePAxSZsGSvFJQtm', '2', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(222) NOT NULL,
+  `username` text NOT NULL,
+  `email` varchar(222) NOT NULL,
+  `password` varchar(222) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(1, 'username', 'pramod.kumar@contriverz.com', '123qwe');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `ci_sessions`
---
-ALTER TABLE `ci_sessions`
-  ADD PRIMARY KEY (`session_id`),
-  ADD KEY `last_activity_idx` (`last_activity`);
 
 --
 -- Indexes for table `tbl_last_login`
@@ -157,16 +140,16 @@ ALTER TABLE `tbl_reset_password`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tbl_roles`
---
-ALTER TABLE `tbl_roles`
-  ADD PRIMARY KEY (`roleId`);
-
---
 -- Indexes for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
   ADD PRIMARY KEY (`userId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -176,25 +159,25 @@ ALTER TABLE `tbl_users`
 -- AUTO_INCREMENT for table `tbl_last_login`
 --
 ALTER TABLE `tbl_last_login`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_reset_password`
 --
 ALTER TABLE `tbl_reset_password`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tbl_roles`
---
-ALTER TABLE `tbl_roles`
-  MODIFY `roleId` tinyint(4) NOT NULL AUTO_INCREMENT COMMENT 'role id', AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `userId` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
